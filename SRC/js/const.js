edd7e5437d04c3e4ce9c4e52735c70d5ef9dd985
yukiya.iseda <202503ap01_iseda@x-ide.sakura.ne.jp>
2025-06-12 11:39:31 +0900

    #40727/工事管理表/【 金額の入力で9を入力するもしくは9桁以上入力するとエラーメッセージが表示される 】【「販売予定額」の入力可能とする桁数が仕様書とエラーメッセージで合っていない　】

--- SRC/js/const.js
+++ SRC/js/const.js
@@ -9,7 +9,7 @@ function fnConstEditCheck() {
 	}
 
 	if (isLength(100, "築年", form.years)) { return; }
-	if (isNumericLength(8, "販売予定額", form.sellPrice)) { return; }
+	if (isNumericLength(9, "販売予定額", form.sellPrice)) { return; }
 	if (isNumericLength(9, "内装見越額", form.interiorPrice)) { return; }
 	if (isLength(100, "施工業者", form.constTrader)) { return; }
 	if (isNumericLength(9, "工事金額", form.constPrice)) { return; }
